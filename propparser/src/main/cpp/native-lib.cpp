1244f806bf5f9d13e7d4be666102c0fb8b27f002
qiang <qiangxliu@tencent.com>
2021-11-18 16:50:29 +0800

    尝试解析 /dev/__properties__/property_info

--- propparser/src/main/cpp/native-lib.cpp
+++ propparser/src/main/cpp/native-lib.cpp
@@ -2,10 +2,13 @@
 #include <string>
 #include<android/log.h>
 #include "./system_properties/include/prop_area.h"
+#include "./property_service/include/property_info_parser.h"
 
 #define TAG    "Q_M" // 这个是自定义的LOG的标识
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGD类型
+
+using android::properties::PropertyInfoAreaFile;
 
 //https://cs.android.com/android/platform/superproject/+/master:system/core/init/property_service.cpp;l=90;bpv=1;bpt=1
 extern "C"
@@ -101,22 +104,57 @@ Java_com_github_propparser_MainActivity_parsePropInMemroy(JNIEnv *env, jobject t
 
         }
     }
-}extern "C"
+}
+
+
+//https://cs.android.com/android/platform/superproject/+/master:bionic/libc/system_properties/system_properties.cpp;drc=master;l=61
+extern "C"
 JNIEXPORT void JNICALL
 Java_com_github_propparser_MainActivity_mapPropInMemroy(JNIEnv *env, jobject thiz) {
+    android::properties::PropertyInfoAreaFile property_info_area_file_;
+//cp /dev/__properties__/property_info /data/local/tmp/property_info
+    property_info_area_file_.LoadPath("/data/local/tmp/property_info");
 
-    prop_area *pa;
-    int s, fd;
-    unsigned sz;
-    char *prop_env;
-    //获取环境变量ANDROID_PROPERTY_WORKSPACE
-    //与上面init进程中设置对应
-    prop_env = getenv("ANDROID_PROPERTY_WORKSPACE");
-    //共享内存文件描述符 内存大小
-    fd = atoi(prop_env);
-    sz = atoi(prop_env + 1);
-    //将文件描述符映射到当前进程虚拟空间内存，实现共享内存
-    pa = static_cast<prop_area *>(mmap(0, sz, PROT_READ, MAP_SHARED, fd, 0));
-    //全局变量指向共享系统属性内存首地址
-    pa->find("ro.serialno");
+    //context 是 "u:object_r:bluetooth_prop:s0"
+    //type 是 类型 string bool ...
+    property_info_area_file_->FindContextIndex("u:object_r:bluetooth_prop:s0");// 4
+
+    auto num_context_nodes = property_info_area_file_->num_contexts();
+    LOGD("property_info_area_file_.num_context_nodes ==> %d", num_context_nodes);
+    for (int i = 0; i < num_context_nodes; ++i) {
+        LOGD("num_context_nodes. ==> %s",
+             property_info_area_file_->context(i));
+    }
+
+    auto num_types = property_info_area_file_->num_types();
+    LOGD("property_info_area_file_.num_types ==> %d", num_types);
+    for (int i = 0; i < num_types; ++i) {
+        LOGD("num_types. ==> %s",
+             property_info_area_file_->type(i));
+    }
+
+
+    //这是一颗树
+    //
+    // +-----+   children    +----+   children    +--------+
+    // |     |-------------->| ro |-------------->| secure |
+    // +-----+               +----+               +--------+
+    //                       /    \                /   |
+    //                 left /      \ right   left /    |  prop   +===========+
+    //                     v        v            v     +-------->| ro.secure |
+    //                  +-----+   +-----+     +-----+            +-----------+
+    //                  | net |   | sys |     | com |            |     1     |
+    //                  +-----+   +-----+     +-----+            +===========+
+    // Represents a node in the trie.
+    const android::properties::TrieNode &rootNode = property_info_area_file_->root_node();
+    int num_child_nodes = rootNode.num_child_nodes();
+    LOGD("rootNode.num_child_nodes ==> %d", num_child_nodes);
+    for (int i = 0; i < num_child_nodes; ++i) {
+
+        LOGD("child_node ==> %s",
+             rootNode.child_node(i).name());
+    }
+
+
+
 }
